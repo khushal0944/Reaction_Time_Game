87687ccafd408de387f1a2860fb8aed3258c481f
Khushal Kumar <146096972+khushal0944@users.noreply.github.com>
2024-06-21 22:47:03 +0530

    Update GridItems.jsx

--- src/components/GridItems.jsx
+++ src/components/GridItems.jsx
@@ -3,8 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { updateBestAvgTime } from "../slices/bestScoreSlice";
 import { useState,useEffect } from "react";
 import { updateAvgTime } from "../slices/scoreSlice";
-import { updateGamesPlayed } from "../slices/userSlice"
-import { updateBoxClicked } from "../slices/userSlice";
+import { updateGamesPlayed, updateBoxClicked } from "../slices/userSlice";
 
 export const GridItems = ({ rows, columns, highlightedIndex, setScore, setEndTime, score, timeArray }) => {
     const navigate = useNavigate()
